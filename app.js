241933549abac5fb59d4939f49ad94b669b4f8bb
unknown <huzefaratlam63@gmail.com>
2025-09-02 14:28:05 +0530

    f

--- app.js
+++ app.js
@@ -23,5 +23,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use('/',(req,res) => {
+    res.status(200).send('hello');
+})
 export default app;
